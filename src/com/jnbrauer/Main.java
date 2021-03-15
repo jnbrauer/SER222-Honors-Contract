be88e2e754f15b342328130ae63bd8cae0bec289
jnbrauer <44125701+jnbrauer@users.noreply.github.com>
2021-03-15 15:58:07 -0700

    3/5 meeting notes

--- src/com/jnbrauer/Main.java
+++ src/com/jnbrauer/Main.java
@@ -5,6 +5,17 @@ import com.jnbrauer.algorithm.Scheduler;
 
 public class Main {
 
+    /**
+     * see what people think of schedules
+     *
+     * read more about how others are using genetic algorithms and representing data
+     *
+     * interval trees
+     *
+     * meet around 18th, some output (random schedules)
+     *
+     * @param args
+     */
     public static void main(String[] args) {
         Scheduler scheduler = new Scheduler();
 
